933638079da3d041c6c4c0f989addba56365a4f7
leo <work.mileo@gmail.com>
2020-09-21 14:29:51 +0800

    test

--- src/auth.js
+++ src/auth.js
@@ -4,6 +4,9 @@ import params from '@/server/params'
 const Key_AuthInfo = 'xq_auth_info'
 let authInfo = storage.get(Key_AuthInfo)
 
+console.log('aaaaa', uni)
+
+
 function setInfo(info) {
   authInfo = info
   storage.set(Key_AuthInfo, info)

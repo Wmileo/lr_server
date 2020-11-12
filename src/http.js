911130b18fe9ff43720ebf0ebe639d80766ef5a8
leo <work.mileo@gmail.com>
2020-11-12 14:21:35 +0800

    api

--- src/http.js
+++ src/http.js
@@ -57,6 +57,45 @@ fly.interceptors.response.use(
   }
 )
 
+class Page {
+
+  constructor(fetch, data, size = 10) {
+    this.fetch = fetch // 请求
+    this.data = data // 查询参数
+    this.size = size // 每页数量
+    this.total = 0 // 总共数量
+    this.num = 1 // 当前第几页
+    this.nums = 0 // 总共页数
+    this.cursor = null // 首次查询时间戳
+    this.isAll = false // 是否拉取完
+  }
+
+  refresh() { // 刷新列表
+    this.cursor = new Date().getTime() / 1000
+    return this.page(1)
+  }
+
+  more() { // 加载更多
+    return this.page(this.num + 1)
+  }
+
+  page(num) { // 第几页
+    let data = Object.assign(this.data, {
+      page: num,
+      pageSize: this.size,
+      cursor: this.cursor
+    })
+    return this.fetch.fetch(data).then(res => {
+      this.total = res.total
+      this.nums = Math.ceil(this.total / this.size)
+      this.num = num
+      this.isAll = this.nums == this.num
+      return res.dataList
+    })
+  }
+
+}
+
 class Fetch {
   constructor(api) {
     this.api = api
@@ -75,10 +114,14 @@ class Fetch {
     }
   }
 
+  page(data, size = 10) {
+    return new Page(this, data, size)
+  }
+
   fetch(data) {
     return this[this.api.type](data)
   }
-  
+
   // 请求
   request(data, options) {
     this.fixPath(data)

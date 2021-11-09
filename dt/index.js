23a3bf5eec655a3ecd2d1c31570a89d23aef6cb1
leo <work.mileo@gmail.com>
2021-11-09 09:22:09 +0800

    update

--- dt/index.js
+++
@@ -1,9 +0,0 @@
-import DtAuth from './DtAuth.js';
-import DtHandle from './DtHandle.js';
-import DtApi from './DtApi.js';
-import Server from '../base/Server.js'
-import dt from '@dt/dt'
-
-let server = new Server(DtApi, DtHandle, DtAuth)
-
-export default server
